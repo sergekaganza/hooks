91da35a8af7cc47a5d6796cd07859cabedf32299
sergekaganza <kaganzas@yahoo.fr>
2020-12-28 08:42:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
+import ThemeContextProvider from './contexts/ThemeContexts';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import  { ThemeContext } from '../contexts/ThemeContexts';
+
+
+class Navbar extends Component  {
+  render() {
+      
+    return (
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark} = context;
+        const theme = isLightTheme ? light : dark;
+        return(
+            <nav style= {{background: theme.ui, color: theme.syntax }}>
+        <h1>Content App</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>  
+        </ul>
+      </nav>
+        )
+      }}</ThemeContext.Consumer>
+      
+    );
+   }
+ }
+
+
+ export default Navbar;
